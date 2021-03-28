0bb380cadfea006c9c76918d11b08b0041ba787b
Fillipeaugusto <fillipeaugustoribeiro@gmail.com>
2021-03-28 19:13:37 -0300

    card component :heavy_check_mark:

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import Footer from './Components/Footer';
 import ArticlesCards from './Components/AticlesCards';
 import Header from './Components/Header';
 import Comments from './Components/Comments';
-
+import { AiFillPushpin } from "react-icons/ai";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ArticlesCards />
+      <section className="main_blog">
+        <header className="main_blog_header">
+          <h1 className="icon-blog"><AiFillPushpin color='#bf350f' /> Nossos últimos artigos</h1>
+          <p>Aqui você encontra os artigos necessários para auxiliar na sua caminhada web</p>
+        </header>
 
-      <article class="main_optin">
-        <div class="main_optin_content">
+        <ArticlesCards />
+        <ArticlesCards />
+        <ArticlesCards />
+        <ArticlesCards />
+        <ArticlesCards />
+        <ArticlesCards />
+        <ArticlesCards />
+        <ArticlesCards />
+      </section>
+      <article className="main_optin">
+        <div className="main_optin_content">
           <header>
             <h1>Quer receber todas as novidades no seu e-mail?</h1>
             <p>Informe seu nome e seu email no campo ao lado e clique em ok!</p>
@@ -26,7 +39,6 @@ function App() {
           </form>
         </div>
       </article>
-
 
       <Comments />
       <Footer />
